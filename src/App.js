bcd210c131dbfc4cef49acd049525643f0c187ee
Atmajaya Dega Wiradiana <atmajaya.dw@gmail.com>
2021-08-18 18:11:50 +0700

    Routing Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Home, About, Portfolio, Contact} from './pages/index.js'
+import {
+  Switch,
+  Route,
+  BrowserRouter
+} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/portfolio" component={Portfolio} exact />
+            <Route path="/contact" component={Contact} exact />
+            <Route path="/about" component={About} exact />
+            {/* <Route path="/portfolio/content" component={Content} exact /> */}
+          </Switch>
+        </BrowserRouter>
+      </>
+    )
+  }
 }
-
-export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home.js'
+import About from './About.js'
+import Portfolio from './Portfolio'
+import Contact from './Contact'
+
+export {Home, About, Portfolio, Contact}
