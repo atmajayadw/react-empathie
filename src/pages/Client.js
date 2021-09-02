a4fd0c47c230293de7f3d10ab400253b8dabf6d9
Atmajaya Dega Wiradiana <atmajaya.dw@gmail.com>
2021-09-02 22:50:03 +0700

    Fixing navbar

--- src/pages/Client.js
+++ src/pages/Client.js
@@ -4,6 +4,30 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import $ from "jquery";
 
+const Loader = () => (
+  <div className="divLoader">
+    <svg className="svgLoader" viewBox="0 0 100 100" width="10em" height="10em">
+      <path
+        stroke="none"
+        d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50"
+        fill="#7E8075"
+        transform="rotate(179.719 50 51)"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          calcMode="linear"
+          values="0 50 51;360 50 51"
+          keyTimes="0;1"
+          dur="1s"
+          begin="0s"
+          repeatCount="indefinite"
+        ></animateTransform>
+      </path>
+    </svg>
+  </div>
+);
+
 export default class Client extends Component {
   constructor(props) {
     super(props);
@@ -16,6 +40,7 @@ export default class Client extends Component {
 
   componentDidMount() {
     this.getData();
+    this.setLoaderOff();
   }
 
   componentDidUpdate() {
@@ -69,6 +94,20 @@ export default class Client extends Component {
     }
   };
 
+  setLoaderOff = () => {
+    document.body.style.overflow = "hidden";
+    setTimeout(() => {
+      const loader = document.querySelector("#loader");
+      loader.style.display = "none";
+      this.setState({
+        loading: false,
+      });
+      document.body.style.overflow = "auto";
+      const jumbotron = document.querySelector("#client");
+      jumbotron.style.opacity = "1";
+    }, 800);
+  };
+
   render() {
     const { result } = this.state;
 
@@ -81,6 +120,14 @@ export default class Client extends Component {
         </div>
 
         <Navbar />
+
+        <div className="container" id="loader">
+          {this.state.loading ? <Loader /> : null}
+          <p>
+            <i>loading...</i>
+          </p>
+        </div>
+
         <section id="client">
           <div className="container">
             <Link
